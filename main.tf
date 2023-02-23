780c3c9547a596789b93a02614db0d99af2f12c4
Koosha Tahmasebipour <kooshi.ml@gmail.com>
2023-02-23 11:38:21 -0500

    VPC & subnet

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region = "ca-central-1"
+}
+
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
+
+    tags = {
+        Name: "${var.env_prefix}-vpc"
+    }
+}
+
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
+
+    tags = {
+      Name = "${var.env_prefix}-subnet-1"
+    }
+}
