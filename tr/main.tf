aa09adad38fe6f04852af265d465143be97b45f2
Koosha Tahmasebipour <kooshi.ml@gmail.com>
2023-02-25 20:17:32 -0500

    subnet module

--- tr/main.tf
+++ tr/main.tf
@@ -10,36 +10,13 @@ resource "aws_vpc" "myapp-vpc" {
     }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
+module "myapp-subnet" {
+    source = "./modules/subnet"
+    subnet_cidr_block = var.subnet_cidr_block
+    avail_zone = var.avail_zone
+    env_prefix = var.env_prefix
     vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.avail_zone
-
-    tags = {
-      Name = "${var.env_prefix}-subnet-1"
-    }
-}
-
-
-resource "aws_internet_gateway" "myapp-igw" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    tags = {
-      Name = "${var.env_prefix}-igw"
-    }
-}
-
-
-resource "aws_default_route_table" "main-rtb" {
     default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
-        route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-
-    tags = {
-      Name = "${var.env_prefix}-main-rtb"
-    }
 }
 
 resource "aws_security_group" "myapp-sg" {
@@ -86,14 +63,6 @@ data "aws_ami" "latest-amazon-linux-image" {
     }
 }
 
-output "aws_ami_id" {
-  value = data.aws_ami.latest-amazon-linux-image.id
-}
-
-output "ec2_public_ip" {
-    value = aws_instance.myapp-server.public_ip
-}
-
 resource "aws_key_pair" "ssh_key" {
     key_name = "server-key"
     public_key = file(var.public_key_location)
@@ -104,7 +73,7 @@ resource "aws_instance" "myapp-server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = var.instance_type
 
-    subnet_id = aws_subnet.myapp-subnet-1.id
+    subnet_id = module.myapp-subnet.subnet.id
     vpc_security_group_ids = [aws_security_group.myapp-sg.id]
     availability_zone = var.avail_zone
 
--- tr/variables.tf
+++ tr/variables.tf
@@ -1,7 +1,6 @@
 
+variable subnet_cidr_block{}
 variable vpc_cidr_block {}
-
-variable subnet_cidr_block {}
 
 variable avail_zone {}
 
