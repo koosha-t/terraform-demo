aa9a0e2c7f0b4d4fa92d8b6a7c810410e5855094
Koosha Tahmasebipour <kooshi.ml@gmail.com>
2023-02-27 09:14:08 -0500

    configuring remote storage with aws s3

--- tr/main.tf
+++ tr/main.tf
@@ -1,3 +1,12 @@
+terraform {
+    required_version = ">= 0.12"
+    backend "s3" {
+      bucket = "myapp-bucket"
+      key = "myapp/state.tfstate"
+      region = "ca-central-1"
+    }
+}
+
 provider "aws" {
     region = "ca-central-1"
 }
